4a200b4c225692d783144d9243623ce63d53f010
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-02-16 15:13:50 +0100

    update

--- src/src/js/task-06.js
+++ src/src/js/task-06.js
@@ -1,16 +1,13 @@
 const input = document.querySelector("#validation-input");
-// const valid = document.querySelector("#validation-input.valid");
-// const invalid = document.querySelector("#validation-input.invalid");
 
 input.addEventListener("blur", (event) => {
   console.log(input.value.length);
-  if (input.value.length <= 6) {
-      input.classList.add("valid");
-      input.classList.remove("invalid");
+  if (input.value.length === 6) {
+    input.classList.add("valid");
+    input.classList.remove("invalid");
     console.log(input.classList);
   } else {
-      input.classList.add("invalid");
-    //   input.classList.remove("valid");
+    input.classList.add("invalid");
     console.log(input.classList);
   }
 });
--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -1,14 +1,15 @@
 const btn = document.querySelector(".change-color");
 const chageColor = document.querySelector(".color");
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
 btn.addEventListener("click", anotherColor);
 function anotherColor() {
-  chageColor.textContent = getRandomHexColor();
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
   document.body.style.backgroundColor = getRandomHexColor();
+  chageColor.textContent = document.body.style.backgroundColor;
 }
 
 anotherColor();

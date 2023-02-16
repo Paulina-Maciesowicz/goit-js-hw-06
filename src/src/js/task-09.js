35068737de762ea4c5b5a199b869dd0e7aaf18d7
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-02-16 15:56:05 +0100

    update task-9

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -1,15 +1,15 @@
 const btn = document.querySelector(".change-color");
 const chageColor = document.querySelector(".color");
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 btn.addEventListener("click", anotherColor);
 function anotherColor() {
-  function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, 0)}`;
-  }
-  document.body.style.backgroundColor = getRandomHexColor();
-  chageColor.textContent = document.body.style.backgroundColor;
+  const color = getRandomHexColor();
+  chageColor.textContent = color;
+  document.body.style.backgroundColor = color;
 }
-
 anotherColor();
